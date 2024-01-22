727858a362dd6317561af7e5df818dddb71ed815
Tiến Dev <designdalat@gmail.com>
2024-01-22 16:12:31 +0700

    home
    home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,12 +14,12 @@ import Team5 from "@/components/sections/Team5"
 import Testimonial5 from "@/components/sections/Testimonial5"
 
 export const metadata = {
-    title: 'Thiết Kế Website Chuyên Nghiệp, Chuẩn SEO Đà Lạt - Lâm Đồng',
+    title: 'Thiết Kế Website Đà Lạt Chuyên Nghiệp, Chuẩn SEO - Lâm Đồng',
     description: 'ICT Đà Lạt là công ty thiết kế website chuyên nghiệp, uy tín tại Đà Lạt, Bảo Lộc.☎️ 0944838788 Dịch vụ Thiết kế website Đà Lạt - Lâm Đồng trọn gói chuẩn seo  2024✔️✔️ Thiết kế web bán hàng cao cấp, bảo trì trọn đời',
     openGraph: {
-        title: 'Thiết Kế Website Chuyên Nghiệp, Chuẩn SEO Đà Lạt - Lâm Đồng',
+        title: 'Thiết Kế Website Đà Lạt Chuyên Nghiệp, Chuẩn SEO - Lâm Đồng',
         description: 'ICT Đà Lạt là công ty thiết kế website chuyên nghiệp, uy tín tại Đà Lạt, Bảo Lộc.☎️ 0944838788 Dịch vụ Thiết kế website Đà Lạt - Lâm Đồng trọn gói chuẩn seo  2024✔️✔️ Thiết kế web bán hàng cao cấp, bảo trì trọn đời',
-        images: '/logo.png',
+        images: 'https://ictdalat.vn/logo.png',
     },
 }
 
